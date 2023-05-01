6b5ee4f7795b552de55dcc32b70752a006df9df9
Cissé <tairou.cisse@axazara.com>
2023-05-01 14:02:23 +0100

    - structure data

---
+++ src/data/data.js
@@ -0,0 +1,33 @@
+export const data = [
+    {
+        id: 1,
+        name: 'John',
+        profession: 'Developer',
+        contact_call: '9988776655',
+        contact_whatsapp: '9988776655',
+        whatsapp_url: 'https://wa.me/22951220980',
+        profile_url: '',
+        category: ''
+
+    },
+    {
+        id: 2,
+        name: 'John',
+        profession: 'Menusier',
+        contact_call: '9988776655',
+        contact_whatsapp: '9988776655',
+        whatsapp_url: 'https://wa.me/22951220980',
+        profile_url: '',
+        category: ''
+    },
+    {
+        id: 3,
+        name: 'John',
+        profession: 'Menusier',
+        contact_call: '9988776655',
+        contact_whatsapp: '9988776655',
+        whatsapp_url: 'https://wa.me/22951220980',
+        profile_url: '',
+        category: ''
+    }
+]
